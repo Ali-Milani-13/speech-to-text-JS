19312065a6d5d5a8611ec3a63e4e2bfe0bb8cf61
Ali Milani <haythamtemplar@hotmail.com>
2022-07-06 12:16:53 +0430

    [MODIFICATION] modify classname "search-box" to "word-box":   index.html         [MODIFICATION] modify voice recongnition language:   js/index.js 	[FEATURE] add glassmorphism and suitable background:   styles/main.css

--- js/index.js
+++ js/index.js
@@ -1,23 +1,31 @@
 {
-
-
-   const content = document.getElementById('content');
-   const button = document.querySelector('.btn-speech');
-   const isSpeechOn = true;
-    window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
-    const recognition = new window.SpeechRecognition();
-    recognition.interimResults = true;
-    recognition.addEventListener('result',e=>{
-        const text = Array.from(e.results)
-        .map(results=>results[0])
-        .map(result=>result.transcript)
-        .join('');
-        content.innerText = text;
-    })
-    button.addEventListener('click',()=>{
-        recognition.start();
-        button.innerText = 'press again to cancel';
-
-    })
-   
+  const content = document.getElementById("content");
+  const button = document.querySelector(".btn-speech");
+  const rec = () => {
+    recognition.start();
+  };
+  window.SpeechRecognition =
+    window.SpeechRecognition || window.webkitSpeechRecognition;
+  const recognition = new window.SpeechRecognition();
+  recognition.interimResults = true;
+  recognition.lang = "fa-IR";
+  recognition.addEventListener("result", (e) => {
+    const text = Array.from(e.results)
+      .map((results) => results[0])
+      .map((result) => result.transcript)
+      .join("");
+    content.innerText = text;
+  });
+  button.addEventListener("click", () => {
+    if (button.innerText === "press to speak") {
+      rec();
+      button.innerText = "press again to cancel";
+      recognition.addEventListener("end", () => {
+        button.innerText = "press to speak";
+      });
+    } else {
+      recognition.stop();
+      button.innerText = "press to speak";
+    }
+  });
 }
