ac4c3efb38d813461548f61189166ad054e04b0a
Ali Milani <haythamtemplar@hotmail.com>
2022-07-06 13:56:01 +0430

    [MODIFICATION] change backround:   assets/image/background.jpg         [MODIFICATION] change button name:   index.html 	[BUGFIX] fix performance issue:   js/index.js 	[MODIFICATION] change radio button label color:   styles/main.css

--- js/index.js
+++ js/index.js
@@ -1,15 +1,31 @@
 {
+  window.onunload = () => {
+    recognition.stop();
+  };
   const content = document.getElementById("content");
   const button = document.querySelector(".btn-speech");
+  const langEnglish = document.getElementById("lang-en");
+  const langPersian = document.getElementById("lang-fa");
+  const langTurkish = document.getElementById("lang-tr");
 
   const rec = () => {
+    if (langEnglish.checked) {
+      recognition.lang = "en-US";
+    } else if (langPersian.checked) {
+      recognition.lang = "fa-IR";
+    } else if (langTurkish.checked) {
+      recognition.lang = "tr-TR";
+    }
     recognition.start();
+    langEnglish.setAttribute("disabled", "");
+    langPersian.setAttribute("disabled", "");
+    langTurkish.setAttribute("disabled", "");
   };
   window.SpeechRecognition =
     window.SpeechRecognition || window.webkitSpeechRecognition;
   const recognition = new window.SpeechRecognition();
   recognition.interimResults = true;
-  recognition.lang = "fa-IR";
+
   recognition.addEventListener("result", (e) => {
     const text = Array.from(e.results)
       .map((results) => results[0])
@@ -18,15 +34,21 @@
     content.innerText = text;
   });
   button.addEventListener("click", () => {
-    if (button.innerText === "press to speak") {
+    if (button.innerText === "Press to speak") {
       rec();
       button.innerText = "press again to cancel";
       recognition.addEventListener("end", () => {
-        button.innerText = "press to speak";
+        button.innerText = "Press to speak";
+        langEnglish.removeAttribute("disabled", "");
+        langPersian.removeAttribute("disabled", "");
+        langTurkish.removeAttribute("disabled", "");
       });
     } else {
       recognition.stop();
-      button.innerText = "press to speak";
+      button.innerText = "Press to speak";
+      langEnglish.removeAttribute("disabled", "");
+      langPersian.removeAttribute("disabled", "");
+      langTurkish.removeAttribute("disabled", "");
     }
   });
 }
